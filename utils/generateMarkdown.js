d090ae80a859ea58b018e35d5b4349074bd55bef
Brittany Boughter <brittany.wood0308@yahoo.com>
2023-04-03 20:46:56 -0700

    function created for the license badges

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,33 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if(!license) {
+    return '';
+  }
 
+  let badge;
+  switch(license) {
+    case 'MIT':
+      badge = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+      break;
+    case 'GNU GPL':
+      badge = '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)';
+      break;
+    case 'Apache':
+      badge = '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+      break;
+    case 'CC-BY-SA':
+      badge = '[![License: CC-BY-SA](https://licensebuttons.net/l/by-sa/3.0/88x31.png)](https://creativecommons.org/licenses/by-sa/3.0/)';
+      break;
+    case 'ISC':
+      badge = '[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)';
+      break;
+    default:
+      badge = '';
+      break;
+  }
+  return badge;
+}
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {}
